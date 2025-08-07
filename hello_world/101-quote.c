e47ca3c8c7ea469baaf5c729ee81c8a3a55339e3
azer <aslan@AsusAzer.localdomain>
2025-08-07 13:48:40 +0400

    C

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,15 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
- *
- * Return: Always 1 (Error)
+ * Return:1
  */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, msg, sizeof(msg) - 1);
-
-	return (1);
+char ch[]
+ = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
+return (1);
 }
