f28334a780273632943b2b421a59f092ca5b77d7
azer <aslan@AsusAzer.localdomain>
2025-08-07 13:41:24 +0400

    C

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,7 +7,10 @@
  */
 int main(void)
 {
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, msg, sizeof(msg) - 1);
+    const char msg_part1[] = "and that piece of art is useful\" - ";
+    const char msg_part2[] = "Dora Korpar, 2015-10-19\n";
+
+    write(2, msg_part1, sizeof(msg_part1) - 1);
+    write(2, msg_part2, sizeof(msg_part2) - 1);
     return (1);
 }
