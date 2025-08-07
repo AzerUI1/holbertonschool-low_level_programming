7d76abe96261c92968828776c707e8b8372eaaca
azer <aslan@AsusAzer.localdomain>
2025-08-07 13:39:37 +0400

    C

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,18 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 
 /**
  * main - Entry point
  *
- * Return: 1 (error)
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-	char quote[] = 
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, quote, strlen(quote));
-
-	return (1);
+    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, msg, sizeof(msg) - 1);
+    return (1);
 }
