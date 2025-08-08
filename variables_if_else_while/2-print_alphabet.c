d7be7c6a65becbf83f0f0f63a39c391dca625174
azer <aslan@AsusAzer.localdomain>
2025-08-08 16:05:51 +0400

    C

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the last digit of a random number and its category
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char n; 
+  for (n = 'a'; n <='z'; n++)
+    putchar(n);
+  putchar('\n');
+  return (0); 
+}
