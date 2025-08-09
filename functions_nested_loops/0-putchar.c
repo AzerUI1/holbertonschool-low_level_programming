f5fb4d9f12e7161743aa72696e30b505c642ca58
AzerUI1 <azer0808aslanov@gmail.com>
2025-08-09 14:48:18 +0400

    C

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _putchar - prints the string "_putchar" followed by a new line
+ *
+ * Return: nothing
+ */
+void _putchar(void)
+{
+	printf("_putchar\n");
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar();
+	return (0);
+}
