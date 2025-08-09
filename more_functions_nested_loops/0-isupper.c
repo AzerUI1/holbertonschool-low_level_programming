64a48e389362bed36a0d6c9e039e4863f0b65fd0
AzerUI1 <azer0808aslanov@gmail.com>
2025-08-09 17:09:32 +0400

    C

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,13 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: the character to check
+ * @c: character to check
+ *
  * Return: 1 if uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-    return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }	
