946b35af75db2c1030a56055414fb13ee940d1ac
AzerUI1 <azer0808aslanov@gmail.com>
2025-08-09 17:30:12 +0400

    C

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit - checks if a character is a digit (0-9)
+ * _isdigit - checks for a digit (0 through 9)
  * @c: character to check
  *
  * Return: 1 if c is a digit, 0 otherwise
