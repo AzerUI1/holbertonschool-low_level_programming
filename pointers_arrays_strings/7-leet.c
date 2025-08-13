5f6487ca96aca1aa495e26e89239276e0c8c8db3
AzerUI1 <azer0808aslanov@gmail.com>
2025-08-13 14:48:16 +0400

    C

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: input string
+ *
+ * Return: pointer to the modified string
+ */
+char *leet(char *s)
+{
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+				break; /* exit inner loop once match is found */
+			}
+		}
+	}
+
+	return (s);
+}
+
