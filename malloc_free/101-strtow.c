ae010124afa8f4c70f72318fc7f9c3101d968594
AzerUI1 <azer0808aslanov@gmail.com>
2025-08-19 15:53:18 +0400

    C

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -2,45 +2,70 @@
 #include <stdlib.h>
 
 /**
+ * word_count - counts the number of words in a string
+ * @str: the string to count words from
+ *
+ * Return: number of words
+ */
+int word_count(char *str)
+{
+	int count = 0, i = 0;
+
+	while (str[i])
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+			count++;
+		i++;
+	}
+
+	return (count);
+}
+
+/**
+ * word_len - returns the length of a word starting at str
+ * @str: the string starting at the word
+ *
+ * Return: length of the word
+ */
+int word_len(char *str)
+{
+	int len = 0;
+
+	while (str[len] && str[len] != ' ')
+		len++;
+
+	return (len);
+}
+
+/**
  * strtow - splits a string into words
  * @str: string to split
  *
- * Return: pointer to an array of strings (words), or NULL on failure
+ * Return: pointer to array of strings, or NULL on failure
  */
 char **strtow(char *str)
 {
 	char **words;
-	int i, j, k, count, len;
+	int i, j, k, wc, len;
 
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
 
-	/* Count words */
-	count = 0;
-	for (i = 0; str[i]; i++)
-		if ((str[i] != ' ' && (i == 0 || str[i - 1] == ' ')))
-			count++;
-
-	if (count == 0)
+	wc = word_count(str);
+	if (wc == 0)
 		return (NULL);
 
-	words = malloc(sizeof(char *) * (count + 1));
+	words = malloc(sizeof(char *) * (wc + 1));
 	if (words == NULL)
 		return (NULL);
 
 	k = 0;
-	for (i = 0; i < count; i++)
+	for (i = 0; i < wc; i++)
 	{
 		while (str[k] == ' ')
 			k++;
 
-		len = 0;
-		j = k;
-		while (str[j] != ' ' && str[j] != '\0')
-		{
-			len++;
-			j++;
-		}
+		len = word_len(str + k);
 
 		words[i] = malloc(sizeof(char) * (len + 1));
 		if (words[i] == NULL)
@@ -57,7 +82,7 @@ char **strtow(char *str)
 		words[i][len] = '\0';
 	}
 
-	words[count] = NULL;
+	words[wc] = NULL;
 
 	return (words);
 }
