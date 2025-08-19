184472da8e35befddd705e7fbc960c1200345ce0
AzerUI1 <azer0808aslanov@gmail.com>
2025-08-19 17:34:33 +0400

    C

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,112 +1,117 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
- * _isdigit - checks if a string contains only digits
- * @s: string to check
- *
- * Return: 1 if all digits, 0 otherwise
- */
-int _isdigit(char *s)
-{
-	int i = 0;
-
-	while (s[i])
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-/**
- * _strlen - returns length of a string
- * @s: string
- *
- * Return: length
+ * _strlen - Calculate the length of a string
+ * @s: The string
+ * Return: Length of the string
  */
 int _strlen(char *s)
 {
-	int len = 0;
-
-	while (s[len])
-		len++;
-	return (len);
+    int len = 0;
+    while (s[len])
+        len++;
+    return (len);
 }
 
 /**
- * errors - prints "Error" and exits with status 98
+ * is_digit_string - Check if a string contains only digits
+ * @s: The string to check
+ * Return: 1 if all digits, 0 otherwise
  */
-void errors(void)
+int is_digit_string(char *s)
 {
-	char *err = "Error\n";
-
-	while (*err)
-	{
-		_putchar(*err);
-		err++;
-	}
-	exit(98);
+    int i = 0;
+    while (s[i])
+    {
+        if (!isdigit(s[i]))
+            return (0);
+        i++;
+    }
+    return (1);
 }
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: always 0
+ * multiply - Multiply two numbers represented as strings
+ * @num1: First number
+ * @num2: Second number
+ */
+void multiply(char *num1, char *num2)
+{
+    int len1 = _strlen(num1);
+    int len2 = _strlen(num2);
+    int len_result = len1 + len2;
+    int i, j, carry, product;
+    int *result;
+    
+    /* Allocate and initialize result array with zeros */
+    result = calloc(len_result, sizeof(int));
+    if (result == NULL)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    
+    /* Perform long multiplication */
+    for (i = len1 - 1; i >= 0; i--)
+    {
+        carry = 0;
+        for (j = len2 - 1; j >= 0; j--)
+        {
+            product = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1] + carry;
+            carry = product / 10;
+            result[i + j + 1] = product % 10;
+        }
+        result[i + j + 1] = carry;
+    }
+    
+    /* Find the first non-zero digit */
+    i = 0;
+    while (i < len_result && result[i] == 0)
+        i++;
+    
+    /* If all digits are zero, the result is zero */
+    if (i == len_result)
+    {
+        printf("0\n");
+    }
+    else
+    {
+        /* Print the result */
+        for (; i < len_result; i++)
+            printf("%d", result[i]);
+        printf("\n");
+    }
+    
+    free(result);
+}
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 on success, 98 on error
  */
 int main(int argc, char *argv[])
 {
-	char *num1, *num2;
-	int len1, len2, len, i, j, carry, n1, n2, *res;
-
-	if (argc != 3)
-		errors();
-
-	num1 = argv[1];
-	num2 = argv[2];
-
-	if (!_isdigit(num1) || !_isdigit(num2))
-		errors();
-
-	len1 = _strlen(num1);
-	len2 = _strlen(num2);
-	len = len1 + len2;
-
-	res = malloc(sizeof(int) * len);
-	if (res == NULL)
-		return (1);
-
-	for (i = 0; i < len; i++)
-		res[i] = 0;
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		n1 = num1[i] - '0';
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			n2 = num2[j] - '0';
-			carry += res[i + j + 1] + (n1 * n2);
-			res[i + j + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry > 0)
-			res[i + j + 1] += carry;
-	}
-
-	i = 0;
-	while (i < len && res[i] == 0)
-		i++;
-	if (i == len)
-		_putchar('0');
-	for (; i < len; i++)
-		_putchar(res[i] + '0');
-	_putchar('\n');
-
-	free(res);
-	return (0);
+    /* Check number of arguments */
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return (98);
+    }
+    
+    /* Check if both arguments are composed of digits only */
+    if (!is_digit_string(argv[1]) || !is_digit_string(argv[2]))
+    {
+        printf("Error\n");
+        return (98);
+    }
+    
+    /* Multiply the numbers */
+    multiply(argv[1], argv[2]);
+    
+    return (0);
 }
-
