4c139dc20bec4fd415e171e5be43352e0f14c362
AzerUI1 <azer0808aslanov@gmail.com>
2025-08-19 14:02:58 +0400

    C

---
+++ recursion/101-wildcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings, allowing '*' in s2
+ * @s1: first string
+ * @s2: second string (may contain '*')
+ *
+ * Return: 1 if strings can be considered identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '\0' && *s1 == '\0')
+		return (1);
+
+	if (*s2 == '*')
+	{
+		/* Skip consecutive '*' */
+		while (*(s2 + 1) == '*')
+			s2++;
+
+		/* '*' matches empty string or one/more chars */
+		if (*(s2 + 1) == '\0')
+			return (1);
+
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
+
+		return (wildcmp(s1, s2 + 1));
+	}
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
+}
